9233c944b58e24ba54feece0eb1dc0b57fd68bdf
Air-KS <kevinrogeret@gmail.com>
2024-04-19 00:31:15 +0200

    App script main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+// ./App.js
+import React from 'react';
+import VideoPlayer from './components/VideoPlayer';
+
+
 import './App.css';
+import './styles/header.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-banner">
+        <div className="banner-content">
+          <div className="left-container">
+          <a href="https://air-ks.github.io/homelaser-game/">
+            <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
+            </a>
+          </div>
+          <div className="right-container">
+            <div className="navigation-links">
+              <a href="#Contactus">Contact us</a>
+              <a href="#Login">Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <VideoPlayer videoId="AKSgdx_VPRQ" />
     </div>
   );
 }
