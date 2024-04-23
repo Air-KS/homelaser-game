362a02082cf85edda3d134f59b64ef4561017a2b
Air-KS <kevinrogeret@gmail.com>
2024-04-23 12:11:04 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// ./App.js
+/* src/App.css */
 import React from 'react';
 import VideoPlayer from './components/VideoPlayer';
 
@@ -8,23 +8,36 @@ import './styles/header.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='header' data-section-theme="dark">
       <div className="top-banner">
-        <div className="banner-content">
+        <div className="header-container">
           <div className="left-container">
-          <a href="https://air-ks.github.io/homelaser-game/">
-            <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
+            <a href="https://air-ks.github.io/homelaser-game/">
+              <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
             </a>
           </div>
-          <div className="right-container">
-            <div className="navigation-links">
-              <a href="#Contactus">Contact us</a>
-              <a href="#Login">Login</a>
-            </div>
+          <div className='right-container'>
+            <nav className='navbar-menu'>
+              <ul className='nav-menu'>
+                <li className='nav-menu-item'>
+                  <a href='#Contact' className='nav-menu-link'>Contact us</a>
+                </li>
+                <li className='nav-menu-item'>
+                  <a href='#Login' className='nav-menu-link'>Login</a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
+
       <VideoPlayer videoId="AKSgdx_VPRQ" />
+
+      <div className="bottom-banner">
+        <div className="bottom-banner-content">
+          {/* Ici, tu peux ajouter du contenu à ton bandeau inférieur si nécessaire */}
+        </div>
+      </div>
     </div>
   );
 }
