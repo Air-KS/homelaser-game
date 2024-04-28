a8d34f3a9ef5687d7a969e007991030c0dd7c8da
Air-KS <kevinrogeret@gmail.com>
2024-04-28 20:06:59 +0200

    add import body

--- src/navigation/login.js
+++ src/navigation/login.js
@@ -1,5 +1,6 @@
 /* src/navigation/login.js */
 import React, { useRef, useEffect } from 'react';
+import '../styles/body.css';
 
 const Login = () => {
   const loginRef = useRef(null);
@@ -11,65 +12,20 @@ const Login = () => {
   }, []);
 
   return (
-    <div ref={loginRef}>
-      <br /> <br/> <br/>
-      <h1>Login</h1>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      <p>Veuillez vous connecter pour accéder à votre compte.</p>
-      {/* Ajoute ici les éléments de la page de connexion */}
+    <div className="conteneurBody"> {/* Ajout du conteneur pour le corps de la page */}
+      <div ref={loginRef}>
+        <br /> <br/> <br/>
+        <h1>Login</h1>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        <p>Veuillez vous connecter pour accéder à votre compte.</p>
+        {/* Ajoute ici les éléments de la page de connexion */}
+      </div>
     </div>
   );
 }
