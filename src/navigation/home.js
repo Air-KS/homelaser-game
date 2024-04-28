80f2a50c2ce41c0aaf92b3743dece37018edcbb8
Air-KS <kevinrogeret@gmail.com>
2024-04-28 20:05:32 +0200

    Add home file js

---
+++ src/navigation/home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='conteneurBody'>
+      <h1>Bienvenue sur notre site</h1>
+      <p>Ceci est la page d'accueil</p>
+    </div>
+  );
+}
+
+export default Home;
