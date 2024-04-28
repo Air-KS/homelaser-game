03c4a18865fd19584281f8ee1e869a01ba898c0b
Air-KS <kevinrogeret@gmail.com>
2024-04-28 16:37:26 +0200

    Create file login.js

---
+++ src/navigation/login.js
@@ -0,0 +1,77 @@
+/* src/navigation/login.js */
+import React, { useRef, useEffect } from 'react';
+
+const Login = () => {
+  const loginRef = useRef(null);
+
+  useEffect(() => {
+    if (loginRef.current) {
+      loginRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }, []);
+
+  return (
+    <div ref={loginRef}>
+      <br /> <br/> <br/>
+      <h1>Login</h1>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      <p>Veuillez vous connecter pour accéder à votre compte.</p>
+      {/* Ajoute ici les éléments de la page de connexion */}
+    </div>
+  );
+}
+
+export default Login;
