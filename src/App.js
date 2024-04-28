6bed71c54c7a681e71d7e2f459681c572a6d3903
Air-KS <kevinrogeret@gmail.com>
2024-04-28 16:36:36 +0200

    Add import Route and Routes for link, and link conact and login file

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
 /* src/App.css */
 import React from 'react';
-import VideoPlayer from './components/VideoPlayer';
-
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 import './App.css';
 import './styles/header.css';
 
+import VideoPlayer from './components/VideoPlayer';
+import Contact from "./navigation/contact";
+import Login from "./navigation/login";
+
 function App() {
   return (
-    <div className='header' data-section-theme="dark">
-      <div className="top-banner">
-        <div className="header-container">
-          <div className="left-container">
-            <a href="https://air-ks.github.io/homelaser-game/">
-              <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
-            </a>
+    <Router>
+      <div className='header' data-section-theme="dark">
+        <div className="top-banner">
+          <div className="header-container">
+            <div className="left-container">
+              <a href="https://air-ks.github.io/homelaser-game/">
+                <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo" className="logo" />
+              </a>
+            </div>
+            <div className='right-container'>
+              <nav className='navbar-menu'>
+                <ul className='nav-menu'>
+                  <li className='nav-menu-item'>
+                    <Link to='/contact' className='nav-menu-link'>Contact us</Link>
+                  </li>
+                  <li className='nav-menu-item'>
+                    <Link to='navigation/login' className='nav-menu-link'>Login</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
-          <div className='right-container'>
-            <nav className='navbar-menu'>
-              <ul className='nav-menu'>
-                <li className='nav-menu-item'>
-                  <a href='#Contact' className='nav-menu-link'>Contact us</a>
-                </li>
-                <li className='nav-menu-item'>
-                  <a href='#Login' className='nav-menu-link'>Login</a>
-                </li>
-              </ul>
-            </nav>
+        </div>
+
+        <VideoPlayer videoId="AKSgdx_VPRQ" />
+
+        <div className="bottom-banner">
+          <div className="bottom-banner-content">
+            {/* Ici, tu peux ajouter du contenu à ton bandeau inférieur si nécessaire */}
           </div>
         </div>
       </div>
 
-      <VideoPlayer videoId="AKSgdx_VPRQ" />
-
-      <div className="bottom-banner">
-        <div className="bottom-banner-content">
-          {/* Ici, tu peux ajouter du contenu à ton bandeau inférieur si nécessaire */}
-        </div>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/navigation/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
