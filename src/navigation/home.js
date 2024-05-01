8db5ce726bd7d55d4372650e071c2720170e0d3c
Air-KS <kevinrogeret@gmail.com>
2024-05-01 23:08:17 +0200

    add RQCODE for mobile

--- src/navigation/home.js
+++ src/navigation/home.js
@@ -7,12 +7,15 @@ import { Link } from 'react-router-dom';
 
 import '../styles/body.css';
 
+import qrcode from '../assets/images/qrcode.png';
+
 const Home = () => {
 
   return (
     <div className='containerBody'>
       <h1>Bienvenue sur notre site</h1>
       <p><Link to='/laserhub'>Visitez LaserHub</Link></p>
+      <img src={qrcode} alt="LaserHub" /> {/* Affiche ton image */}
     </div>
   );
 }
